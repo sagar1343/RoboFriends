b0b8aff25a681ec87a56c931bdf7779caf782fa2
Sagar <sagarchakrawarti24@gmail.com>
2023-08-06 18:32:37 +0530

    improving filter code

--- src/component/RoboMap/RoboMap.js
+++ src/component/RoboMap/RoboMap.js
@@ -1,15 +1,6 @@
 import React from "react";
 import "./Style.css";
 const RoboMap = ({ users, enterValue }) => {
-  const allRobo = users.map((user) => (
-    <div key={user.id} className="robot" id={"robot" + user.id}>
-      <img
-        src={"https://robohash.org/" + user.id}
-        alt={"Robo_img_" + user.id}
-      ></img>
-      <p className="userName">{user.name}</p>
-    </div>
-  ));
   const filteredRobo = users.filter((user) => {
     return user.name.toLowerCase().includes(enterValue.toLowerCase());
   });
@@ -22,11 +13,7 @@ const RoboMap = ({ users, enterValue }) => {
       <p className="userName">{filteredRobo.name}</p>
     </div>
   ));
-  return (
-    <div className="container">
-      {enterValue === "" ? allRobo : mapFilterRobo}
-    </div>
-  );
+  return <div className="container">{mapFilterRobo}</div>;
 };
 
 export default RoboMap;
